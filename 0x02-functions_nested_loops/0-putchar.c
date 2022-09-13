c01bc50f5c8881b5208a92e7bb27ff09354f1a34
Fazal <fazgit0@gmail.com>
2022-09-12 18:11:40 -0700

    Header File

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "Fazal.h"
+/**
+ * main - Prints "Fazal" followed by a new line
+ *
+ * Return: Always 0.
+ *
+ **/
+int main(void)
+{
+_putchar('F');
+_putchar('a');
+_putchar('z');
+_putchar('a');
+_putchar('l');
+_putchar('\n');
+
+return (0);
+}
